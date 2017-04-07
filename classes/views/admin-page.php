508daa0a5e1b6c725ac48a5cc215d83b9015d0f1
Chris Christoff <chriscct7@users.noreply.github.com>
2017-04-07 11:41:34 -0700

    Remove AMP Analytics options from Admin

--- classes/views/admin-page.php
+++ classes/views/admin-page.php
@@ -14,7 +14,6 @@ $yform->admin_header( true, 'wpseo_amp', false, 'wpseo_amp_settings' );
 	<h2 class="nav-tab-wrapper" id="wpseo-tabs">
 		<a class="nav-tab" id="posttypes-tab" href="#top#posttypes"><?php echo esc_html( __( 'Post types', 'wordpress-seo' ) ); ?></a>
 		<a class="nav-tab" id="design-tab" href="#top#design"><?php echo esc_html( __( 'Design', 'wordpress-seo' ) ); ?></a>
-		<a class="nav-tab" id="analytics-tab" href="#top#analytics"><?php echo esc_html( __( 'Analytics', 'wordpress-seo' ) ); ?></a>
 	</h2>
 
 	<div class="tabwrapper">
@@ -114,36 +113,6 @@ $yform->admin_header( true, 'wpseo_amp', false, 'wpseo_amp_settings' );
 				'cols' => 100
 			) ); ?>
 
-		</div>
-
-		<div id="analytics" class="wpseotab">
-			<h2><?php echo esc_html( __( 'AMP Analytics', 'wordpress-seo' ) ); ?></h2>
-
-			<?php
-			if ( class_exists( 'Yoast_GA_Options' ) ) {
-				echo '<p>', esc_html( __( 'Because your Google Analytics plugin by Yoast is active, your AMP pages will also be tracked.', 'wordpress-seo' ) ), '<br>';
-				$UA = Yoast_GA_Options::instance()->get_tracking_code();
-				if ( $UA === null ) {
-					echo esc_html( __( 'Make sure to connect your Google Analytics plugin properly.', 'wordpress-seo' ) );
-				} else {
-					echo sprintf( esc_html( __( 'Pageviews will be tracked using the following account: %s.', 'wordpress-seo' ) ), '<code>' . $UA . '</code>' );
-				}
-
-				echo '</p>';
-
-				echo '<p>', esc_html( __( 'Optionally you can override the default AMP tracking code with your own by putting it below:', 'wordpress-seo' ) ), '</p>';
-				$yform->textarea( 'analytics-extra', __( 'Analytics code', 'wordpress-seo' ), array(
-					'rows' => 5,
-					'cols' => 100
-				) );
-			} else {
-				echo '<p>', esc_html( __( 'Optionally add a valid google analytics tracking code.', 'wordpress-seo' ) ), '</p>';
-				$yform->textarea( 'analytics-extra', __( 'Analytics code', 'wordpress-seo' ), array(
-					'rows' => 5,
-					'cols' => 100
-				) );
-			}
-			?>
 		</div>
 	</div>
 
